5a955f71e3b72e46faa4f3946ed15dde1923bde0
ppby <salswnss@gmail.com>
2020-07-16 14:02:39 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,47 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({ name, picture }) {
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image:
+      'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+    rating: 5.0,
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image:
+      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    name: 'Bibimbap',
+    image:
+      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
+    rating: 2.0,
+  },
+  {
+    id: 4,
+    name: 'Doncasu',
+    image:
+      'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
+    rating: 3.5,
+  },
+  {
+    id: 5,
+    name: 'Kimbap',
+    image:
+      'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
+    rating: 4.2,
+  },
+];
+
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
       <img src={picture} alt={name} />
+      <h5>{rating} / 5.0</h5>
     </div>
   );
 }
-
-const foodILike = [
-  {
-    name: 'Kimchi',
-    image:
-      'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-  },
-  {
-    name: 'Samgyeopsal',
-    image:
-      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
-  },
-  {
-    name: 'Bibimbap',
-    image:
-      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
-  },
-  {
-    name: 'Doncasu',
-    image:
-      'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
-  },
-  {
-    name: 'Kimbap',
-    image:
-      'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
-  },
-];
+Food.prototype = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired, // type을 체크하고 requireed 도 체크
+};
 
 function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food
+          name={dish.name}
+          picture={dish.image}
+          key={dish.id}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
